f51be5a742cbdb9d1c451de16cd9eb07a327b8d3
jisu <93499095+amisu1203@users.noreply.github.com>
2021-12-28 14:55:42 +0900

    버튼 클릭시 인풋 결과 출력

--- 일만시간의 법칙/js/10000hours.js
+++ 일만시간의 법칙/js/10000hours.js
@@ -1,1 +1,11 @@
-// js file
+// 인풋창 출력 함수
+function showResult() {
+    const job = document.getElementById('inpJob').value;
+    const hour = document.getElementById('inpHour').value;
+    let perHour = 10000/(parseInt(hour));
+    document.getElementById('jobValue').textContent = job;
+    document.getElementById('hourValue').textContent = Math.ceil(perHour);
+    document.querySelector('.outputArea').scrollIntoView(true);
+}
+const btnSubmit = document.querySelector('.btnSubmit');
+btnSubmit.addEventListener('click', showResult);
